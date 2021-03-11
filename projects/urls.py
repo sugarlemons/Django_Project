361669fee27a44e3284e68822ff85fde30ca9382
XiaYWilson <xiayuwilson@github.com>
2021-03-11 10:57:24 +0800

    first edition
    first edition

---
+++ projects/urls.py
@@ -0,0 +1,50 @@
+"""project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from workapp import views # 在这里添加这一行代码，导入 views
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', views.index),
+    path('index.html', views.sub0),
+    path('charts.html', views.sub1),
+    path('panels.html', views.sub2),
+    path('notifications.html', views.sub3),
+    path('page-lockscreen.html', views.sub4),
+    path('page-login.html', views.sub5),
+    path('page-profile.html', views.sub6),
+    path('typography.html', views.sub7),
+    path('elements.html',views.sub8),
+    path('tables.html',views.sub9),
+    path('icons.html',views.sub10),
+    path('page-charts.html',views.sub11),
+    path('test.html',views.sub12)
+
+
+
+    # path('upload1', views.upload1), # 人脸识别
+    # path('upload2', views.upload2), # 场景分类
+    # path('upload3', views.upload3), # 目标检测
+    # path('upload4', views.upload4), # 视频异常检测
+    # path('upload5', views.upload5), # 视频目标跟踪
+    # path('upload6', views.upload6), # 语音识别
+
+]
---
+++ workapp/views.py
@@ -0,0 +1,117 @@
+import os
+from django.shortcuts import render, HttpResponse
+import  json
+# Create your views here.
+
+# 添加 index 函数，返回 index.html 页面
+def index(request):
+    return render(request, 'index.html')
+
+def sub0(request):
+    return render(request, 'index.html')
+
+def sub1(request):
+    return render(request, 'charts.html')
+
+def sub2(request):
+    return render(request, 'panels.html')
+
+def sub3(request):
+    return render(request, 'notifications.html')
+
+def sub4(request):
+    return render(request, 'page-lockscreen.html')
+
+def sub5(request):
+    return render(request, 'page-login.html')
+
+def sub6(request):
+    view_name = "Joe Bidden"
+    view_relationship = [["妻子","吉尔·拜登"],
+                    ["儿子","亨特·拜登"],
+                    ["儿子","傅·拜登"],
+                    ["弟弟","詹姆斯·拜登"]]#关系不固定有几个，而且可能有好几个儿子，所以用字典的时候可能会有重名的问题
+    view_label = {"sports":0,
+             "foods":0,
+             "politic":1,
+             "military":1}#这些标签是固定的，用0/1代表有/没有，
+
+
+    # chart_options = {
+    #     'title': 'ECharts 入门示例',
+    #     'legend':  ['销量'],
+    #     'xAxis:': ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+    #     'series': ['销量', 'bar',[5, 20, 36, 10, 10, 20]] #name type data
+    # }#传不过去
+    #
+    # result = {"name":json.dumps(view_name),
+    #           "relationship":json.dumps(view_relationship) ,#前端直接relationship.key的名字即可提取
+    #           "label":json.dumps(view_label),
+    #           "option":json.dumps(chart_options),
+    #           }#传递复杂数据对象不行, 无法解析
+
+
+
+    #echarts表格数据，无法整合到一个大的集合里，无法解析，目前只能一个一个传入
+    title = 'ECharts 入门示例'
+    legend = ['销量']
+    xAxis = ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+    series = ['销量', 'bar',[5, 20, 36, 10, 10, 20]] #传递复杂数据对象不行
+
+    title2 = {
+               'text': '某站点用户访问来源',
+               'subtext': '纯属虚构',
+               'left': 'center'
+           },
+
+    itemstyle = {'itemStyle': {
+                            'shadowBlur': 10,
+                            'shadowOffsetX': 0,
+                            'shadowColor': 'rgba(0, 0, 0, 0.5)'
+                        }}# 无法解析
+
+    data = [
+              {'value': 1048, 'name': '搜索引擎'},
+            {'value': 735, 'name': '直接访问'},
+            {'value': 580, 'name': '邮件营销'},
+            {'value': 484, 'name': '联盟广告'},
+            {'value': 300, 'name': '视频广告'}
+              ]#
+    data_value = [ 1048, 735, 580, 484, 300]
+    data_name = ['搜索引擎','直接访问','邮件营销','联盟广告','视频广告']
+
+    result = {"name":json.dumps(view_name),
+              "relationship":json.dumps(view_relationship) ,#前端直接relationship.key的名字即可提取
+              "label":json.dumps(view_label),
+              "title":json.dumps(title),
+              "legend":json.dumps(legend),
+              "xAxis":json.dumps(xAxis),
+              "series":json.dumps(series),
+              "title2":json.dumps(title2),
+              "datavalue":json.dumps(data_value),
+              "dataname":json.dumps(data_name)
+              }
+
+    return render(request, 'page-profile.html', result)
+
+def sub7(request):
+    return render(request, 'typography.html')
+
+def sub8(request):
+    return render(request, 'elements.html')
+
+def sub9(request):
+    return render(request, 'tables.html')
+
+def sub10(request):
+    return render(request, 'icons.html')
+
+def sub11(request):
+    return render(request, 'page-charts.html')
+
+def sub12(request):
+    data = ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+    return render(request, 'test.html',{'datas':json.dumps(data)})
+
+# def upload1(request):
+#     return render(request, 'sub1.html')
